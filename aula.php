5ba378bf4d1f873158f65983aab5e3e79c018e49
Bruno2808 <brunorc_tamboara@hotmail.com>
2020-03-02 09:56:38 -0300

    noticias

---
+++ aula.php
@@ -0,0 +1,55 @@
+  <!DOCTYPE html>
+  <html lang="pt-br" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+
+  <h2>Portal de Notícias</h2>
+
+  <div class="noticias">
+    <img src="https://s03.video.glbimg.com/x720/6702918.jpg">
+  <p> Prédio explode em São Paulo </p>
+  <p> Suspeitos de terem colocado explosivos no local foram abordados pela polícia nessa tarde de segunda-feira </p>
+  <p> Foram encaminhados até a delegacia e aguardam por entrevista </p>
+
+  </div>
+
+
+
+      <div class="noticias">
+  <img src="https://tonobuteco.com.br/wp-content/uploads/2020/02/expo-paranavai-pr-2020-shows.jpg" >
+  <p> Cinco shows na Expo Paranavaí 2020 </p>
+  <p> Segue abaixo </p>
+        <table>
+      <tr>
+        <th>Jorge e Mateus</th>
+        <th>06/03</th>
+      </tr>
+      <tr>
+        <td>Naiara Azevedo e Lauana Prado</td>
+        <td>07/03</td>
+      </tr>
+      <tr>
+        <td>Marília Mendonça</td>
+        <td>13/03</td>
+      </tr>
+      <tr>
+        <td>Zezé Di Camargo e Luciano</td>
+        <td>14/03</td>
+      </tr>
+    </table>
+      </div>
+
+
+
+
+      <div class="noticias">
+      <img src="https://img.freepik.com/fotos-gratis/mulher-asiatica-de-negocios-assinando-um-documento-de-contrato-fazendo-um-acordo_1421-670.jpg?size=626&ext=jpg" >
+  <p >Moradora de Tamboara assume Tabelionato de Notas nessa segunda-feira</p>
+  <p> Cláudia formada em direito após assumir cartório contrata amigo para trabalharem juntos </p>
+      </div>
+
+    </body>
+  </html>
