0507fa0df7d9b58c490a18dfb822b23f81c2483e
Aluno <brunorc_tamboara@hotmail.com>
2020-03-09 09:32:14 -0300

    aula04

---
+++ aula04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS  e HTML</title>
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+<link rel="stylesheet" href="aula04.css">
+<link href="https://fonts.googleapis.com/css?family=Bellota&display=swap" rel="stylesheet">
+  </head>
+  <body>
+<div class="site">
+
+  <div class="topo">
+Bagaceira
+</div>
+    <div class="conteudo">
+      Corpo do site
+    </div>
+    <div class="rodape">
+      Desenvolvido por Bruno
+    </div>
+    </div>
+  </body>
+</html>
