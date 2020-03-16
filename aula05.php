8a1058b0571e9f28ef693b2dbcff6cd263a63f4a
Bruno2808 <brunorc_tamboara@hotmail.com>
2020-03-16 10:04:56 -0300

    aula05

---
+++ aula05.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS  e HTML</title>
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+    <link href="https://fonts.googleapis.com/css?family=Bellota&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="aula05.css">
+  </head>
+  <body>
+<div class="site">
+
+  <div class="topo">
+<h3>Criação de Menu e Botões<h3>
+
+  <div class="menu">
+    <ul>
+      <li><a href="#">Página Inical</a></li>
+      <li><a href="#">Cadastro</a></li>
+      <li><a href="#">Contato</a></li>
+      <li><a href="#">Ajuda</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
+
+</div>
+    <div class="conteudo">
+      <h3>Criação de Botões<h3>
+        <a href="#" class= "botao botao-azul">Botão Azul</a>
+        <a href="#" class= "botao botao-verde">Botão Verde</a>
+        <a href="#" class= "botao botao-vermelho">Botão Vermelho</a>
+
+        <body>
+
+        <h2>Portal de Notícias</h2>
+
+        <div class="noticias">
+        <img src="https://s03.video.glbimg.com/x720/6702918.jpg">
+        <p> Prédio explode em São Paulo </p>
+        <p> Suspeitos de terem colocado explosivos no local foram abordados pela polícia nessa tarde de segunda-feira </p>
+        <p> Foram encaminhados até a delegacia e aguardam por entrevista </p>
+
+        </div>
+
+
+
+          <div class="noticias">
+        <img src="https://tonobuteco.com.br/wp-content/uploads/2020/02/expo-paranavai-pr-2020-shows.jpg" >
+        <p> Cinco shows na Expo Paranavaí 2020 </p>
+        <p> Segue abaixo </p>
+            <table>
+          <tr>
+            <th>Jorge e Mateus</th>
+            <th>06/03</th>
+          </tr>
+          <tr>
+            <td>Naiara Azevedo e Lauana Prado</td>
+            <td>07/03</td>
+          </tr>
+          <tr>
+            <td>Marília Mendonça</td>
+            <td>13/03</td>
+          </tr>
+          <tr>
+            <td>Zezé Di Camargo e Luciano</td>
+            <td>14/03</td>
+          </tr>
+        </table>
+          </div>
+          <div class="noticias">
+          <img src="https://img.freepik.com/fotos-gratis/mulher-asiatica-de-negocios-assinando-um-documento-de-contrato-fazendo-um-acordo_1421-670.jpg?size=626&ext=jpg" >
+      <p >Moradora de Tamboara assume Tabelionato de Notas nessa segunda-feira</p>
+      <p> Cláudia formada em direito após assumir cartório contrata amigo para trabalharem juntos </p>
+          </div>
+
+        </body>
+      </html>
+
+    </div>
+    <div class="rodape">
+      Desenvolvido por Bruno
+    </div>
+    </div>
+  </body>
+</html>
